9f40dc373b35d0294b995fb6829076f25744d1e9
TheKhaeng <nonthawit.kub@gmail.com>
2017-04-30 18:09:01 +0700

    [+] add TheKhaeng splashscreen

---
+++ app/src/main/java/com/thekhaeng/recyclerviewmargindecoration/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.thekhaeng.recyclerviewmargindecoration;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate( @Nullable Bundle savedInstanceState ){
+        super.onCreate( savedInstanceState );
+        Handler handler = new Handler();
+        handler.postDelayed( new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent( SplashActivity.this, MainActivity.class );
+                startActivity( intent );
+                finish();
+                overridePendingTransition( R.anim.fade_in, R.anim.fade_out );
+            }
+        }, 400);
+    }
+}
